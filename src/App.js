57a7786e1460b2ce6aa14bc502362a19e3dfff0a
AusDietrich <33529775+ASP-React-Database@users.noreply.github.com>
2020-09-17 12:40:39 -0700

    just like from aws

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = { status:true};
+	}
+	render() {
+		const displayMessage = this.state.status ? (
+			<div>
+                  <p><h3>The Application status is now - Completed.</h3></p>
+            </div>
+              ) :  (
+                  <div>
+                      <p><h3>The Application status is currently - Inprogress.</h3></p>
+                </div>
+                  ) ;
+              return (
+                  <div>
+                      <h1> Welcome to Demo Application </h1>
+                      { displayMessage }
+                  </div>
+              );
+     }
 }
-
 export default App;
